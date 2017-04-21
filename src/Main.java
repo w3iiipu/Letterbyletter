ab38cc84302b8cefadef282c6146f6f2d036476d
w3iiipu <w3iiipu@gmail.com>
2017-04-21 19:32:48 -0400

    added check for equal string length

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        String line = in.readLine();
+        System.out.println("Please enter two equal length strings: ");
+        String line1 = in.readLine();
         String line2 = in.readLine();
 
-        for (int i = 0; i <= line.length(); i++) {
+        if (line1.length() != line2.length()) {
+            System.out.println("Strings are not equal in length! Please enter the strings again.");
+            line1 = in.readLine();
+            line2 = in.readLine();
+        }
 
-            String newName = line2.substring(0,i) +" "+line.substring(i);
+        for (int i = 0; i <= line1.length(); i++) {
+
+            String newName = line2.substring(0, i) + " " + line1.substring(i);
             System.out.println(newName);
         }
 
